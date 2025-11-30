aab05589d2190e3a92adcebdf2b3b3ad25bff6e5
FadlixArdiansyah <fadliardi65@gmail.com>
2025-11-30 23:03:51 +0700

    Menambahkan ADT

--- Tugas-Besar_IF4803_kelompok_13/Komposer.h
+++ Tugas-Besar_IF4803_kelompok_13/Komposer.h
@@ -1,5 +1,31 @@
 #ifndef KOMPOSER_H_INCLUDED
 #define KOMPOSER_H_INCLUDED
+#include <iostream>
+using namespace std;
+
+typedef string infotypeP;
+typedef struct elmParent *addressP;
+
+struct elmParent {
+    infotypeP infoK;
+    addressP next;
+    addressP nextChild;
+};
+
+struct ListParent {
+    addressP first;
+};
+
+void createListParent(ListParent &L);
+addressP createElemenParent(infotypeP x);
+void insertFirstParent(ListParent &L, addressP p);
+void insertLastParent(ListParent &L, addressP p);
+void insertAfterParent(ListParent &L, addressP p, addressP prec);
+void deleteFirstParent(ListParent &L, addressP &p);
+void deleteLastParent(ListParent &L, addressP &p);
+void deleteAfterParent(ListParent &L, addressP &p, addressP prec);
+addressP findElemenParent(ListParent L, infotypeP x);
+void viewParent(ListParent L);
 
 
 
--- Tugas-Besar_IF4803_kelompok_13/Musik.h
+++ Tugas-Besar_IF4803_kelompok_13/Musik.h
@@ -1,6 +1,28 @@
 #ifndef MUSIK_H_INCLUDED
 #define MUSIK_H_INCLUDED
 
+using namespace std;
 
+typedef string infotypeC;
+
+typedef struct elmChild *addressC;
+
+struct elmParent {
+    infotypeC info;
+    addressC next;
+    addressC prev;
+};
+
+void createListChild(addressP p);
+addressC createElemenChild(infotypeC x);
+void insertFirstChild(addressP p, addressC c);
+void insertLastChild(addressP p, addressC c);
+void insertAfterChild(addressC prec, addressC c);
+void deleteFirstChild(addressP p, addressC &c);
+void deleteLastChild(addressP p, addressC c);
+void deleteAfterChild(addressC prec, addressC c);
+addressC findElemenChild(addressP p, infotypeC x);
+void viewChild(addressP p)
 
 #endif // MUSIK_H_INCLUDED
+
