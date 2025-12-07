230c54812e0a9581533e65ee16cd66ed8442afcf
achakhansa24-web <achakhansa24@gmail.com>
2025-12-07 21:48:15 +0700

    Update komposer_103012400131.cpp

--- Tugas-Besar_IF4803_kelompok_13/komposer_103012400131.cpp
+++ Tugas-Besar_IF4803_kelompok_13/komposer_103012400131.cpp
@@ -4,17 +4,45 @@
 
 using namespace std;
 
+void createListParent(ListParent &L){
+    L.first = nullptr;
+}
+
 void insertFirstParent(ListParent &L, addressP P){
+   p-> next = L.first;
+    L.first = p;
 }
 
 void insertAfterParent(ListParent &L, addressP prec, addressP P){
+   if (prec != nullptr){
+        P->next = prec->next;
+        prec->next = P;
+    }
 }
 
 void deleteFirstParent(ListParent &L, addressP &P){
+  if (L.first != nullptr){
+        P = L.first;
+        L.first = P->next;
+        p->next = nullptr;
+    }
 }
 
 void deleteAfterParent(ListParent &L, addressP prec, addressP &P){
+  if (prec != nullptr && prec->next != nullptr){
+        P = prec->next;
+        prec->next = P->next;
+        P->next = nullptr;
+    }
 }
 
 void viewParent(ListParent L){
+  if (L.first == nullptr){
+        cout << "Tidak ada komposer. \n";
+    }else{
+        addressP P =L.first;
+        while (P!= nullptr){
+            cout << "ID: " << P->infoK.ID << " | Nama: " << P->infoK.nama << endl;
+            P = P->next;
+        }
 }
