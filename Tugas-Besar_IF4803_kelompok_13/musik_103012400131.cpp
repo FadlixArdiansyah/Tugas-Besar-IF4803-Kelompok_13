6a4c61c5d1dec7413a7d02dbbd7e80d9cc23a725
achakhansa24-web <achakhansa24@gmail.com>
2025-12-07 22:17:57 +0700

    Update musik_103012400131.cpp

--- Tugas-Besar_IF4803_kelompok_13/musik_103012400131.cpp
+++ Tugas-Besar_IF4803_kelompok_13/musik_103012400131.cpp
@@ -1,16 +1,50 @@
 #include "Musik"
 #include <iostream>
 
+void createListChild(addressP p){
+    P =nullptr;
+}
 void insertFirstChild(addressC &first, addressC C){
-
+  if (first == nullptr) {
+        first = C;
+    } else {
+        addressC last = first;
+        while (last->next != nullptr) {
+            last = last->next;
+        }
+        last->next = C;
+        C->prev = last;
+    }
 }
 
 void insertAfterChild(addressC prec, addressC C){
+  if (prec != nullptr) {
+        C->next = prec->next;
+        prec->next = C;
+    }
 }
 
 void deleteFirstChild(addressC &first, addressC &C){
+  if (first == nullptr) {
+        C = nullptr;
+    } else {
+        C = first;
+        first = first->next;
+        C->next = nullptr;
+    }
 }
 
 void viewChild(addressC first) {
-
+  addressC P = first;
+    if (P == nullptr) {
+        cout << "List Musik kosong.\n";
+    } else {
+        while (P != nullptr) {
+            cout << "ID Musik      : " << P->info.ID << endl;
+            cout << "Judul         : " << P->info.Judul << endl;
+            cout << "Tahun Rilis   : " << P->info.tahunRilis << endl;
+            cout << "---------------------------\n";
+            P = P->next;
+        }
+    }
 }
